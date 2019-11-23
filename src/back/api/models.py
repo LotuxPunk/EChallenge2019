295318c3d664e952ff1ffddb714ab0b10db94605
AlexandreCasteels <47348239+AlexandreCasteels@users.noreply.github.com>
2019-11-23 13:35:33 +0100

    student + suite models

--- src/back/api/models.py
+++ src/back/api/models.py
@@ -10,8 +10,26 @@ class Teacher(models.Model):
 class Group(models.Model):
 	name = models.CharField(max_length = 30)
 	teacher = models.OneToOneField(Teacher, on_delete = models.CASCADE)
+	name = models.CharField(max_length = 30)
 
 class Student(models.Model):
 	user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 	first_name = models.CharField(max_length = 30)
 	group = models.OneToOneField(Group, on_delete = models.CASCADE)
+	email = models.EmailField(null = True)
+	code = models.CharField(max_length = 100)
+
+class Quest(models.Model):
+	subject = models.CharField(max_length = 100)
+	nb_words = models.IntegerField()
+	xp_gain = models.IntegerField()
+	group = models.OneToOneField(Group, on_delete = models.CASCADE)
+
+class HomeWork(models.Model):
+	text = models.CharField(max_length = 10000)
+	is_valide = models.BooleanField()
+	students = models.ManyToManyField(Student, on_delete = models.CASCADE, related_name='+')
+
+class Tag(models.Model):
+	name = models.CharField(max_length = 30)
+	students = models.ManyToManyField(Student, on_delete = models.CASCADE, related_name='+')
