ec8961e74256a2c7b5ed9e54c502be6439963e37
AlexandreCasteels <47348239+AlexandreCasteels@users.noreply.github.com>
2019-11-23 01:19:30 +0100

    model prof

--- src/back/api/models.py
+++ src/back/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Professeur(models.Model):
+	nom_ecole = model.CharField(max_length = 30)
+	verification = model.BooleanField(edfault = True)
+	user = models.ForeignKey(User,null = True, on_delete=models.CASCADE)
